cb9f38be97c89328a5dc3e050145843874bf88c8
AbhayTiwari007 <abhaytiwari037@gmail.com>
2021-05-14 12:24:01 +0530

    initial

--- app/src/main/java/com/example/tasknewsapp/Adapters/Adapter.kt
+++ app/src/main/java/com/example/tasknewsapp/Adapters/Adapter.kt
@@ -1,20 +1,23 @@
 package com.example.tasknewsapp.Adapters
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.tasknewsapp.Models.Data
 import com.example.tasknewsapp.R
+import com.example.tasknewsapp.View.WebViewActvity
 
 class Adapter(val items: Data, val context: Context) : RecyclerView.Adapter<Adapter.ViewHolder>() {
 
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    class ViewHolder(itemView: View,context: Context) : RecyclerView.ViewHolder(itemView) {
         val title: TextView
         val desc: TextView
         val img: ImageView
@@ -27,11 +30,17 @@ class Adapter(val items: Data, val context: Context) : RecyclerView.Adapter<Adap
 
             img = itemView.findViewById(R.id.img)
 
+img.setOnClickListener {
+    val intent = Intent(context,WebViewActvity::class.java)
+    intent.putExtra("key", "Kotlin")
+   intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+    context.startActivity(intent)
+}
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(LayoutInflater.from(context).inflate(R.layout.itemhome, parent, false))
+        return ViewHolder(LayoutInflater.from(context).inflate(R.layout.itemhome, parent, false),context)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
@@ -41,6 +50,7 @@ class Adapter(val items: Data, val context: Context) : RecyclerView.Adapter<Adap
         load(items.articles[position].urlToImage).
         error(R.drawable.logo).
         into(holder.img);
+
 
     }
 
